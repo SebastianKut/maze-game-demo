59b34cb5d403029a5168299470fd191801096bbb
sebastiankut <sebastian_kut@yahoo.co.uk>
2024-02-07 18:18:41 +1000

    initial commit

---
+++ src/Boot.js
@@ -0,0 +1,21 @@
+class BootComponent {
+	constructor(game) {}
+
+	preload() {
+		this.load.image('preloaderBg', 'img/loading-bg.png');
+		this.load.image('preloaderBar', 'img/loading-bar.png');
+	}
+
+	create() {
+		this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.game.scale.pageAlignHorizontally = true;
+		this.game.scale.pageAlignVertically = true;
+		this.game.state.start('Preloader');
+	}
+}
+
+const Ball = {
+	_WIDTH: 320,
+	_HEIGHT: 480,
+	Boot: BootComponent
+};
---
+++ src/Howto.js
@@ -0,0 +1,13 @@
+class HowtoComponent {
+	constructor(game) {}
+
+	create() {
+		this.buttonContinue = this.add.button(0, 0, 'screen-howtoplay', this.startGame, this);
+	}
+
+	startGame() {
+		this.game.state.start('Game');
+	}
+}
+
+Ball.Howto = HowtoComponent;
