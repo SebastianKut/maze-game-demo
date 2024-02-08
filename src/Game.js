06e46e9c31faa256dcbfcd38912764b2d473aa85
sebastiankut <sebastian_kut@yahoo.co.uk>
2024-02-08 18:30:39 +1000

    reversed changes

--- src/Game.js
+++ src/Game.js
@@ -82,6 +82,14 @@ class GameComponent {
     this.keys = this.game.input.keyboard.createCursorKeys();
 
     Ball._player = this.ball;
+    this.permissionButton = this.add.button(
+      Ball._WIDTH - this.pauseButton.width - 8 * 16,
+      8,
+      'button-pause',
+      this.askMotionPermit,
+      this
+    );
+    this.pauseButton.anchor.set(1, 0);
     // window.addEventListener("deviceorientation", this.handleOrientation, true);
 
     this.time.events.loop(Phaser.Timer.SECOND, this.updateCounter, this);
@@ -251,6 +259,39 @@ class GameComponent {
   render() {
     // this.game.debug.body(this.ball);
     // this.game.debug.body(this.hole);
+  }
+
+  handleOrientation(e) {
+    // Device Orientation API
+    var x = e.gamma; // range [-90,90], left-right
+    var y = e.beta; // range [-180,180], top-bottom
+    var z = e.alpha; // range [0,360], up-down
+    Ball._player.body.velocity.x += x;
+    Ball._player.body.velocity.y += y * 0.5;
+  }
+
+  askMotionPermit() {
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+      DeviceOrientationEvent.requestPermission()
+        .then((permissionState) => {
+          if (permissionState === 'granted') {
+            window.addEventListener(
+              'deviceorientation',
+              this.handleOrientation,
+              true
+            );
+          }
+        })
+        .catch(console.error);
+    } else {
+      // handle regular non iOS 13+ devices
+      console.log('Non IOS device');
+      window.addEventListener(
+        'deviceorientation',
+        this.handleOrientation,
+        true
+      );
+    }
   }
 }
 
--- src/Howto.js
+++ src/Howto.js
@@ -12,38 +12,7 @@ class HowtoComponent {
   }
 
   startGame() {
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      DeviceOrientationEvent.requestPermission()
-        .then((permissionState) => {
-          if (permissionState === 'granted') {
-            window.addEventListener(
-              'deviceorientation',
-              this.handleOrientation,
-              true
-            );
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-      console.log('Non IOS device');
-      window.addEventListener(
-        'deviceorientation',
-        this.handleOrientation,
-        true
-      );
-    }
-
     this.game.state.start('Game');
-  }
-
-  handleOrientation(e) {
-    // Device Orientation API
-    var x = e.gamma; // range [-90,90], left-right
-    var y = e.beta; // range [-180,180], top-bottom
-    var z = e.alpha; // range [0,360], up-down
-    Ball._player.body.velocity.x += x;
-    Ball._player.body.velocity.y += y * 0.5;
   }
 }
 
